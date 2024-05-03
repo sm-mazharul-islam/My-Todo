9cf142c4677c55223f040d9c5da59f80e02bfbff
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2024-05-03 13:49:09 +0600

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>My Todo </h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
